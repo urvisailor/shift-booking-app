8f1f3ace06c54f6bf765d4d7230a3bf816e2c4b5
urvisailor <urvisailor@gmail.com>
2024-05-18 11:34:39 +0530

    styles

--- app/screens/availableshifts/styles.js
+++ app/screens/availableshifts/styles.js
@@ -1,5 +1,25 @@
 import { StyleSheet } from "react-native";
+import { COLORS } from "../../constants/colors";
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    headerBg: {
+        backgroundColor: COLORS.WHITE,
+        padding: 20
+    },
+    headerText: {
+        fontWeight: 'bold',
+        fontSize: 15
+    },
+    safeArea: {
+        flex: 1,
+        backgroundColor: "white",
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    }
+})
 
 export default styles
